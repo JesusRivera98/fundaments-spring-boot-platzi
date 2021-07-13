30b22b9b2d2153218921eb52f60498f9472bfcf3
Jesús Rivera Flores <Jesus.RiveraF98@gmail.com>
2021-07-13 17:11:48 -0500

    Class 10: Create project structure

---
+++ src/test/java/com/platzifundaments/springboot/fundaments/FundamentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.platzifundaments.springboot.fundaments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundamentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
