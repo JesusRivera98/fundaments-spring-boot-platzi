e78e1bd5eaabe9598ec04f6ba0f925cc462ee7f1
Jesús Rivera Flores <Jesus.RiveraF98@gmail.com>
2021-07-14 11:23:23 -0500

    Class 21:Using value annotation to point to properties

--- src/main/java/com/platzifundaments/springboot/fundaments/configuration/GeneralConfiguration.java
+++ src/main/java/com/platzifundaments/springboot/fundaments/configuration/GeneralConfiguration.java
@@ -9,10 +9,12 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 import javax.sql.DataSource;
 
 @Configuration
+@PropertySource("classpath:connection.properties")
 @EnableConfigurationProperties(UserPojo.class)
 public class GeneralConfiguration {
     @Value("${value.name}")
@@ -22,7 +24,19 @@ public class GeneralConfiguration {
     private String surname;
 
     @Value("${value.random}")
-    private String randomValue;
+    private String random;
+
+    @Value("${jdbc.url}")
+    private String jdbcUrl;
+
+    @Value("${driver}")
+    private String driver;
+
+    @Value("${username}")
+    private String username;
+
+    @Value("${password}")
+    private String password;
 
     @Bean
     public MyBeanWithProperties myBean(){
@@ -32,10 +46,10 @@ public class GeneralConfiguration {
     @Bean
     public DataSource dataSource() {
         DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
-        dataSourceBuilder.driverClassName("org.h2.Driver");
-        dataSourceBuilder.url("jdbc:h2:mem:test");
-        dataSourceBuilder.username("SA");
-        dataSourceBuilder.password("");
+        dataSourceBuilder.driverClassName(driver);
+        dataSourceBuilder.url(jdbcUrl);
+        dataSourceBuilder.username(username);
+        dataSourceBuilder.password(password);
 
         return dataSourceBuilder.build();
     }
