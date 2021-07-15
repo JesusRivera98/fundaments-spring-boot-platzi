4fb2991be41fb6dd97b156859ed5b78a7f4bdf91
Jesús Rivera Flores <Jesus.RiveraF98@gmail.com>
2021-07-15 09:08:11 -0500

    Class 30: Pagination with Spring Boot

--- src/main/java/com/platzifundaments/springboot/fundaments/controller/UserRestController.java
+++ src/main/java/com/platzifundaments/springboot/fundaments/controller/UserRestController.java
@@ -5,6 +5,8 @@ import com.platzifundaments.springboot.fundaments.caseuse.DeleteUser;
 import com.platzifundaments.springboot.fundaments.caseuse.GetUser;
 import com.platzifundaments.springboot.fundaments.caseuse.UpdateUser;
 import com.platzifundaments.springboot.fundaments.entity.User;
+import com.platzifundaments.springboot.fundaments.repository.UserRepository;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -19,12 +21,14 @@ public class UserRestController {
     private CreateUser createUser;
     private DeleteUser deleteUser;
     private UpdateUser updateUser;
+    private UserRepository userRepository;
 
-    public UserRestController(GetUser getUser, CreateUser createUser, DeleteUser deleteUser, UpdateUser updateUser) {
+    public UserRestController(GetUser getUser, CreateUser createUser, DeleteUser deleteUser, UpdateUser updateUser, UserRepository userRepository) {
         this.getUser = getUser;
         this.createUser = createUser;
         this.deleteUser = deleteUser;
         this.updateUser = updateUser;
+        this.userRepository = userRepository;
     }
 
     @GetMapping("/")
@@ -46,6 +50,11 @@ public class UserRestController {
     @PutMapping("/{id}")
     ResponseEntity<User> replaceUser(@RequestBody User newUser, @PathVariable Long id){
         return new ResponseEntity<>(updateUser.update(newUser, id), HttpStatus.OK);
+    }
+
+    @GetMapping("/pageable")
+    List<User> getUserPageable(@RequestParam int page, @RequestParam int size){
+        return userRepository.findAll(PageRequest.of(page, size)).getContent();
     }
 
 }
--- src/main/java/com/platzifundaments/springboot/fundaments/repository/UserRepository.java
+++ src/main/java/com/platzifundaments/springboot/fundaments/repository/UserRepository.java
@@ -5,6 +5,7 @@ import com.platzifundaments.springboot.fundaments.entity.User;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
@@ -13,7 +14,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends PagingAndSortingRepository<User, Long> {
     @Query("SELECT u FROM User u WHERE u.email=?1")
     Optional<User> findByUserEmail(String email);
 
@@ -58,6 +59,8 @@ public interface UserRepository extends JpaRepository<User, Long> {
     @Query("select u from User u where u.name = :name or u.email = :email")
     Optional<User> findUByNameOrEmail(@Param("name") String name,
                                      @Param("email") String email);
+
+    List<User> findAll();
 
 
 }
